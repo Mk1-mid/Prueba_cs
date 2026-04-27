a7971f4bef2b923f62ae03ec6357b0280bf4a982
Miguel <Callerestrepomigueldavid@gmail.com>
2026-04-27 15:45:25 -0500

    add menu

--- Desempeno/Program.cs
+++ Desempeno/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿static void Main(string[] args)
+{
+    string opcion;
+    do
+    {
+        Console.Clear();
+        Console.WriteLine("--- SISTEMA DE RESERVAS DEPORTIVAS ---");
+        Console.WriteLine("1. Gestión de Usuarios");
+        Console.WriteLine("2. Gestión de Espacios Deportivos");
+        Console.WriteLine("3. Gestión de Reservas");
+        Console.WriteLine("4. Salir");
+        Console.Write("Seleccione una opción: ");
+        opcion = Console.ReadLine();
+
+        switch (opcion)
+        {
+            case "1": MenuUsuarios(); break;
+            case "2": MenuEspacios(); break;
+            case "3": MenuReservas(); break;
+            case "4": Console.WriteLine("Saliendo..."); break;
+            default: Console.WriteLine("Opción inválida."); break;
+        }
+    } while (opcion != "4");
+}
