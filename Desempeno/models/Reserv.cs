cdef3f84d1dd7190cb0700c7fe16557af506057b
Miguel <Callerestrepomigueldavid@gmail.com>
2026-04-27 15:17:00 -0500

    uptadate services

--- Desempeno/models/Reserv.cs
+++ Desempeno/models/Reserv.cs
@@ -5,6 +5,7 @@ public class Reserv
     public int Id { get; set; }
     public int IdUser { get; set; }
     public int IdSpace { get; set; }
+    public string status { get; set; }
     public DateTime Date { get; set; }
     public TimeSpan strat { get; set; }
     public TimeSpan end { get; set; }
---
+++ Desempeno/services/gestion.cs
@@ -0,0 +1,54 @@
+namespace Desempeno.services;
+
+public class gestion
+{
+    public void UserRegistration()
+    {
+        
+    }
+
+    public void UserLogin()
+    {
+        
+    }
+
+    public void Userlist()
+    {
+        
+    }
+    
+    public void Useredit()
+    {
+        
+    }
+    public void UserDelete()
+    {
+        
+    }
+    
+    public void SpaceRegistration()
+    {
+    }
+
+    public void Spacelist()
+    {
+        
+    }
+    public void SpaceEdit()
+    {
+    }
+    public void SpaceDelete()
+    {
+    }
+
+    public void reservRegister()
+    {
+        
+    }
+    public void reservEdit()
+    {
+    }
+    public void reservDelete()
+    {
+    }
+}
